1d81833e6236a904d39905d932a626ef6f9b3a53
Shreyas Gavhalkar <shreyas@pop-os.localdomain>
2022-09-26 04:02:49 +0530

    added a constituency table and candidates table

--- back_end/sona/endpoints_admin/models.py
+++ back_end/sona/endpoints_admin/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+
+class constituency(models.Model):
+    constituency_id = models.IntegerField(primary_key=True)
+    constituency_name = models.TextField()
 class election(models.Model):
     TYPE_OF_ELECTION = [
         ('LS', 'Lok Sabha'),
@@ -10,5 +14,14 @@ class election(models.Model):
     details = models.Textfield()
     start_date = models.DateField(null=False)
     end_date = models.DateField(null=False)
-    constituency_id = models.IntegerField(null=False)
+    constituency_id = models.ForeignKey(constituency)
     admin_id = models.IntegerField(null=False)
+
+class candidates(models.Model):
+    candidate_id = models.IntegerField(primary_key=True)
+    sex = models.CharField(max_length=1, null=False)
+    name = models.TextField(null=False)
+    age = models.IntegerField(null=False)
+    constituency_id = models.ForeignKey(constituency)
+    party = models.TextField(null=False)
+    election_id = models.ForeignKey(election)
