634c75318f7a3cb9bb70f5aa9fa9765a38dee87f
Shreyas Gavhalkar <shreyas@pop-os.localdomain>
2022-09-26 04:08:33 +0530

    added a voter model

--- back_end/sona/endpoints_user/models.py
+++ back_end/sona/endpoints_user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from endpoints_admin.models import constituency
 
+class voter(models.Model):
+    voter_id = models.CharField(max_length=10, primary_key=True)
+    name = models.TextField(null=False)
+    date_of_birth = models.DateField(null=False)
+    sex = models.CharField(max_length=1, null=False)
+    address = models.TextField(null=False)
+    constituency = models.ForeignKey(constituency)
 # Create your models here.
