22058e2a8ec5b73e8efaa2afd271615fe9679c87
Shreyas Gavhalkar <shreyas@pop-os.localdomain>
2022-09-26 03:35:13 +0530

    added election model

--- back_end/sona/endpoints_admin/models.py
+++ back_end/sona/endpoints_admin/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+
+class election(models.Model):
+    TYPE_OF_ELECTION = [
+        ('LS', 'Lok Sabha'),
+        ('RS', 'Rajya Sabha')
+    ]
+    election_id = models.IntegerField(primary_key=True)
+    type = models.CharField(max_length=2, choices = TYPE_OF_ELECTION, null=False)
+    details = models.Textfield()
+    start_date = models.DateField(null=False)
+    end_date = models.DateField(null=False)
+    constituency_id = models.IntegerField(null=False)
+    admin_id = models.IntegerField(null=False)
